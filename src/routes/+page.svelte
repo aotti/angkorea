34cccf1bb906a790ecfedac726425bd3ace22816
aotti <fattahjundi99@gmail.com>
2025-11-10 21:12:16 +0700

    add dummy data for schedules

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,17 @@
-<script lang="ts">
-    const stationList = ['Stasiun A', 'Stasiun B', 'Stasiun C']
+<script lang="ts"> 
+    import schedule from "$lib/assets/config/schedule.json"
+    const portraitClass = `rounded-2xl md:w-[35%] md:mx-auto lg:w-[35%] lg:mx-auto h-screen`
+    const scheduleList = schedule.list
+    // states
+    let currentRoute = $state(0)
     let currentStation = $state(0)
+
+    // station
+    // svelte-ignore state_referenced_locally
+    let stationList = $state([] as any[])
+    $effect(() => {
+        stationList = scheduleList[currentRoute].route.split('-')
+    })
 
     type MouseEventType = Event & {currentTarget: EventTarget & HTMLButtonElement}
     function changeStation(ev: MouseEventType) {
@@ -11,7 +22,7 @@
                 currentStation = currentStation <= 0 ? 0 : currentStation - 1
                 break
             case 'right':
-                currentStation = currentStation >= stationList.length-1 ? 2 : currentStation + 1
+                currentStation = currentStation >= stationList.length-1 ? stationList.length-1 : currentStation + 1
                 break
         }
     }
@@ -29,10 +40,14 @@
         const select = ev.currentTarget
         select.classList.remove('text-black')
         select.blur()
+        // update current route
+        switch (select.value) {
+            case 'A-B': currentRoute = 0; break
+            case 'B-C': currentRoute = 1; break
+            case 'C-D': currentRoute = 2; break
+            default: currentRoute = 0; break
+        }
     }
-
-    import schedule from "$lib/assets/config/schedule.json"
-    const scheduleList = schedule.list
 </script>
 
 <!-- html head -->
@@ -41,20 +56,20 @@
 </svelte:head>
 
 <main class="bg-black/50">
-    <div class="flex flex-col gap-4 bg-dark-blue-2 text-white border border-transparent md:border-black lg:border-black md:w-[35%] md:mx-auto lg:w-[35%] lg:mx-auto h-screen">
+    <div class={`flex flex-col gap-4 bg-dark-blue-2 text-white shadow-xl shadow-black ${portraitClass}`}>
         <!-- title -->
         <div class="text-center py-1 border-b">
             <h1 class="text-xl"> ANGKOREA </h1>
             <i class="text-sm"> jadwal bis bukan angkot </i>
         </div>
 
-        <!-- search -->
+        <!-- find schedule -->
         <div class="p-1">
             <form class="flex justify-around" onsubmit={ev => ev.preventDefault()}>
                 <!-- time input -->
                 <div>
                     <label for="time_input"> waktu </label>
-                    <select id="time_input" class="w-32 border rounded-md px-1 text-center" 
+                    <select id="time_input" class="w-32 border rounded-md px-1 text-center cursor-pointer" 
                     onfocus={focusTimeOrRoute} onblur={blurTimeOrRoute} onchange={changeTimeOrRoute}>
                         <option value="7"> 07:00 </option>
                         <option value="8"> 08:00 </option>
@@ -65,11 +80,15 @@
                 <!-- route input -->
                 <div>
                     <label for="route_input"> rute </label>
-                    <select id="route_input" class="w-32 border rounded-md px-1 text-center" 
+                    <select id="route_input" class="w-32 border rounded-md px-1 text-center cursor-pointer" 
                     onfocus={focusTimeOrRoute} onblur={blurTimeOrRoute} onchange={changeTimeOrRoute}>
-                        <option value="a-b"> A ke B </option>
+                        <!-- <option value="a-b"> A ke B </option>
                         <option value="b-c"> B ke C </option>
-                        <option value="c-d"> C ke D </option>
+                        <option value="c-d"> C ke D </option> -->
+                        {#each scheduleList as s}
+                        {@const routeName = s.route.replace('-', ' ke ')}
+                            <option value={s.route}> {routeName} </option>
+                        {/each}
                     </select>
                 </div>
             </form>
@@ -82,16 +101,23 @@
                 <p class=""></p>
                 <div class="station flex justify-between col-span-3">
                     <button id="station_left" class="hover:cursor-pointer" onclick={changeStation}> ◀ </button>
-                    <p> {stationList[currentStation]} </p>
+                    <p> {`Stasiun ${stationList[currentStation]}`} </p>
                     <button id="station_right" class="hover:cursor-pointer" onclick={changeStation}> ▶ </button>
                 </div>
                 <!-- bus -->
-                <p class="bus"> Bis 1 </p>
-                <p class="col-span-3"> jadwal </p>
-                <p class="bus"> Bis 2 </p>
-                <p class="col-span-3"> jadwal </p>
-                <p class="bus"> Bis 3 </p>
-                <p class="col-span-3"> jadwal </p>
+                {#if scheduleList[currentRoute].route === stationList.join('-')}
+                {@const currentSchedule = scheduleList[currentRoute]}
+                    {#each currentSchedule.buses as bus, i}
+                        <p class="bus"> {currentSchedule.buses[i]} </p>
+                        <p class="schedule col-span-3">
+                            {#if currentStation === 0}
+                                {`jalan ${currentSchedule.depart[i]}`}
+                            {:else}
+                                {`tiba ${currentSchedule.arrive[i]}`}
+                            {/if}
+                        </p>
+                    {/each}
+                {/if}
             </div>
         </div>
     </div>
